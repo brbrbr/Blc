313b636da4ba3fa83f0e71e690a2f643b0b58d20
bram <github@brambring.nl>
2025-06-02 13:30:59 +0200

    Remove old files

--- pkg_blc/script.php
+++ pkg_blc/script.php
@@ -28,12 +28,12 @@ use Joomla\Filesystem\File;
 use Joomla\Filesystem\Folder;
 use Joomla\Filesystem\Path;
 
-return new class () implements ServiceProviderInterface {
+return new class() implements ServiceProviderInterface {
     public function register(Container $container)
     {
         $container->set(
             InstallerScriptInterface::class,
-            new class () implements InstallerScriptInterface {
+            new class() implements InstallerScriptInterface {
                 /**
                  * Minimum  Joomla version to check
                  *
@@ -158,8 +158,8 @@ return new class () implements ServiceProviderInterface {
                     '/administrator/components/com_blc/src/Blc/BlcExtractController.php', //moved to BlcParseController
 
                     //25.44.6807
-                    '/administrator/components/com_blc/src/Traits/BlcSetAltTrait.php', //renamed during DEV
-                    '/administrator/components/com_blc/src/Interface/BlcSetAltInterface.php', //renamed during DEV
+                    '/administrator/components/com_blc/src/Traits/BlcReplaceAltTrait.php', //renamed during DEV
+                    '/administrator/components/com_blc/src/Interface/BlcReplaceAltInterface.php', //renamed during DEV
 
 
 
