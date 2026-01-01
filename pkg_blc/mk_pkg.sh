f3ca68eeacb7179a1967963e1ebecc725fe6e41f
bram <github@brambring.nl>
2026-01-01 08:51:24 +0100

    Sync 25.44.8031

--- pkg_blc/mk_pkg.sh
+++ pkg_blc/mk_pkg.sh
@@ -60,7 +60,7 @@ cat >  /var/www/downloads.brokenlinkchecker.dev/$base.xml << EOM
                 <maintainerurl>https://brambring.nl</maintainerurl>
                 <section>-</section>
                 <changelogurl>https://downloads.brokenlinkchecker.dev/changelog-pkg_blc.xml</changelogurl>
-		<targetplatform name="joomla" version="(5.(0|1|2|3)|4.(4))|6\.(0|1)"/>
+		<targetplatform name="joomla" version="(5.(0|1|2|3|4)|4.(4))|6\.(0|1)"/>
                 <php_minimum>8.2</php_minimum>
  		<supported_databases mysql="8.0.13" mariadb="10.2.1"/>
         </update>
